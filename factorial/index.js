3c2eb1b3ac6b8872a567538891841b3f47d70992
Jesse <jesse.morgan@hotmail.co.uk>
2022-06-14 16:01:30 +0100

    stuff

--- factorial/index.js
+++ factorial/index.js
@@ -29,5 +29,24 @@ function facrev(fact){
         };
     }
 }
+
+function doggo(x){
+    for(let i = 0; i <= 100; i++){
+        if (i == x){
+            i++;
+        };
+        if(i == 1 || i == 21 || i ==  31 || i ==  41 || i ==  51 || i ==  61 || i ==  71 || i ==  81 || i ==  91){
+            console.log(`${i}st`);
+        }else if(i == 2 || i == 22 || i ==32 || i == 42 || i == 52 || i == 62|| i == 72 || i == 82 || i == 92){
+            console.log(`${i}nd`);
+        }else if(i == 3 || i ==23 || i == 33 || i == 43 || i == 53 || i == 63 || i == 73 || i == 83 || i == 93){
+            console.log(`${i}rd`);
+        }else{
+            console.log(`${i}th`);
+        }
+        
+    }
+}
 fac(5);
 facrev(120);
+doggo(3)
--- factorial/test.js
+++ factorial/test.js
@@ -1,15 +1,14 @@
 const mocha = require('mocha');
 const chai = require('chai');
-const fac = require('./index');
+const facto = require('./index');
 
-mocha.describe('Test factorial', () => {
-  mocha.it('should equal 5', () => {
-    chai.expect(5).to.equal(fac.facrev(120));
-  });
-  mocha.it('should equal 4', () => {
-    chai.expect(6).to.equal(fac.facrev(720));
-  });
-  mocha.it.skip('should equal 10', () => {
-    chai.expect(4).to.equal(fac.facrev(22));
+mocha.describe('index', () => {
+  mocha.describe('facrev', () => {
+    mocha.it('should equal 5', () => {
+      chai.expect(5).to.equal(facto.facrev(120));
+    });
+    mocha.it('should equal 6', () => {
+      chai.expect(facto.facrev(720)).to.equal(6);
+    });
   });
 });
