989ade238d0d594f08cdc4d23705616e72cf36b7
Jesse <jesse.morgan@hotmail.co.uk>
2022-06-15 10:00:24 +0100

    tests

--- factorial/index.js
+++ factorial/index.js
@@ -6,14 +6,10 @@ function fac(fact) {
     console.log(`The factorial of ${fact}! is... `);
     for (let i = 1 ; i <= fact ; i++ ){
         res = res*i;
-        if(fact==1){
-            console.log(`${i}!`);
-            return i;
-            break;
-        };
     };
     console.log(res);
-}
+    return res;
+};
 
 function facrev(fact){
     console.log(`The factorial of ${fact} is... `);
@@ -27,26 +23,37 @@ function facrev(fact){
             return j;
             break;
         };
-    }
-}
+    };
+};
 
 function doggo(x){
-    for(let i = 0; i <= 100; i++){
+    let s1 = "st";
+    let s2 = "nd";
+    let s3 = "rd";
+    let s4 = "th";
+    const array = [];
+    for(let i = 1; i <= 100; i++){
         if (i == x){
             i++;
         };
         if(i == 1 || i == 21 || i ==  31 || i ==  41 || i ==  51 || i ==  61 || i ==  71 || i ==  81 || i ==  91){
-            console.log(`${i}st`);
+            array.push(`${i}${s1}`);
         }else if(i == 2 || i == 22 || i ==32 || i == 42 || i == 52 || i == 62|| i == 72 || i == 82 || i == 92){
-            console.log(`${i}nd`);
+            array.push(`${i}${s2}`);
         }else if(i == 3 || i ==23 || i == 33 || i == 43 || i == 53 || i == 63 || i == 73 || i == 83 || i == 93){
-            console.log(`${i}rd`);
+            array.push(`${i}${s3}`);
         }else{
-            console.log(`${i}th`);
-        }
-        
-    }
-}
-fac(5);
-facrev(120);
-doggo(3)
+            array.push(`${i}${s4}`);
+        };
+    };
+    for(i = 0; i < array.length; i++){
+        console.log(array[i]);
+    };
+    return array[x-1];
+};
+
+module.exports = {
+    fac,
+    facrev,
+    doggo
+};
--- factorial/test.js
+++ factorial/test.js
@@ -2,13 +2,27 @@ const mocha = require('mocha');
 const chai = require('chai');
 const facto = require('./index');
 
-mocha.describe('index', () => {
-  mocha.describe('facrev', () => {
-    mocha.it('should equal 5', () => {
-      chai.expect(5).to.equal(facto.facrev(120));
-    });
-    mocha.it('should equal 6', () => {
-      chai.expect(facto.facrev(720)).to.equal(6);
-    });
+mocha.describe('facrev', () => {
+  mocha.it('should equal 5', () => {
+    chai.expect(5).to.equal(facto.facrev(120));
+  });
+  mocha.it('should equal 6', () => {
+    chai.expect(facto.facrev(720)).to.equal(6);
+  });
+});
+mocha.describe('fac', () => {
+  mocha.it('should equal 120', () => {
+    chai.expect(120).to.equal(facto.fac(5));
+  });
+  mocha.it('should equal 720', () => {
+    chai.expect(facto.fac(6)).to.equal(720);
+  });
+});
+mocha.describe('doggo', () => {
+  mocha.it('should equal 6th', () => {
+    chai.expect(`6th`).to.equal(facto.doggo(5));
+  });
+  mocha.it('should equal 21st', () => {
+    chai.expect(`21st`).to.equal(facto.doggo(20));
   });
 });
